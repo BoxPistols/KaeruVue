d3061cb90fea8bca7b2fc65cd724227097287e52
ait <ait@ex.in>
2020-07-09 19:11:20 +0900

    [Props] base

---
+++ src/views/props-child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>PropsChild: {{ nums }}</p>
+  </div>
+</template>
+<script>
+
+  export default {
+    name: "",
+    props:[ "nums" ],
+    data: () => ({
+    }),
+  }
+</script>
+<style lang="scss" scoped="scoped">
+</style>
---
+++ src/views/props-parent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>this nums {{ nums }}</h2>
+    <PropsChild nums="15" />
+    <PropsChild nums="10" />
+    <PropsChild :nums="nums" />
+  </div>
+</template>
+
+<script>
+import PropsChild from "./props-child";
+
+export default {
+  name: "",
+  data: () => ({
+    nums: 12
+  }),
+  components:{
+    PropsChild
+  }
+}
+</script>
+<style lang="scss" scoped="scoped">
+</style>
